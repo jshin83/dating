45b8d81e101cdffc96a5e3de8780c7d1d4a8c70f
Jen Shin <jshin13@mail.greenriver.edu>
2018-01-16 15:27:15 -0500

    added view

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
  * Time: 11:59 AM
  */
 
-//define page1 route
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+
+
+//create an instance of Base class
+$f3 = Base::instance();
+
+$f3->set('DEBUG', 3);
+
+//define page route
 $f3->route('GET /', function() {
     //echo '<h1>This is default</h1>'; //testing purposes
 
