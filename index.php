9a600567c40deb4abfa229ca6fbbebd1ab4d7702
Jen Shin <jshin13@mail.greenriver.edu>
2018-01-17 22:56:07 -0500

    completed default route and testing

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
 <?php
-/**
+/*
  * Created by PhpStorm.
  * User: JenShin
  * Date: 1/16/18
  * Time: 11:59 AM
+ * index.php for dating website
  */
 
 //require the autoload file
 require_once ('vendor/autoload.php');
-
-
 
 //create an instance of Base class
 $f3 = Base::instance();
 
 $f3->set('DEBUG', 3);
 
-//define page route
+//set default path to page/home
 $f3->route('GET /', function() {
-    //echo '<h1>This is default</h1>'; //testing purposes
-
-    $view = new View();
-    echo $view -> render('pages/home.hmtl');
+    $view = new Template();
+    echo $view -> render('pages/home.html');
 }
 );
+//run fat free
+$f3->run();
